0d6f0603562744ec71c80091944cc600dfe5ec98
soube01 <109072070+soube01@users.noreply.github.com>
2022-07-11 15:17:17 +0530

    Add files via upload

---
+++ add_sub_mult_div.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+//#include<conio.h>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"Enter Ist number="<<endl;
+    cin>>a;
+    cout<<"Enter 2nd number="<<endl;
+    cin>>b;
+    cout<<"a+b="<<a+b<<endl;
+    cout<<"a-b="<<a-b<<endl;
+    cout<<"a*b="<<a*b<<endl;
+    cout<<"a/b="<<(float)a/b<<endl;
+    return 0;
+}
